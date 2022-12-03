394da5d1e9e9f238bace0f9e0bc7f2ce419c3005
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Air.local>
2022-12-03 16:43:43 -0500

    Added buttons and cards to home page

--- interactive-ecommerce-dashboard/src/pages/Ecommerce.jsx
+++ interactive-ecommerce-dashboard/src/pages/Ecommerce.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { Stacked, Pie, Button, sparkLine } from "../components"
+import { earningData, SparkLinedata, ecomPieChartDat } from '../data/dummy';
+import  { usedStateContext } from "../contexts/ContextProvider"
 
 const Ecommerce = () => {
   return (
-    <div>Ecommerce</div>
+    <div className="mt-12 ">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+          <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg
+          h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 
+          bg-no-repeat bg-gradient-to-r from-purple-500 to-pink-500 
+           bg-cover bg-center'>
+            <div className='flex justify-between items-center'>
+              <div>
+                <p className='font-bold text-white '>Earnings</p>
+                <p className='text-2xl9 text-white'>$63,448.78</p>
+              </div>
+            </div>
+          <div className='mt-6 '>
+            <Button
+            color="white"
+            bgColor = "blue"
+            text="Download"
+            borderRadius="10px"
+            size="md"
+            />
+          </div>
+          </div>
+          <div className='flex m-3 flex-wrap justify-center gap-1 items-center '>
+            {earningData.map((item) => (
+              <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 
+              p-4 pt-9 rounded-2xl"
+              >
+                <button type="button" 
+                style={{color: item.iconColor,
+                backgroundColor: item.iconBg}} 
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl">
+                   {item.icon}
+                   </button>
+                   <p className='mt-3'>
+                    <span className="text-lg fonr-semibold"> {item.amount}</span>
+                    <span className={`text-sm text-${item.pcColor} ml-2`}>{item.percentage}</span>
+                    
+                   </p>
+                
+            </div>
+            ))}
+          </div>
+        </div>
+    
+    </div>
   )
 }
 
