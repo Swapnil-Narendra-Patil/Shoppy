f2145ba6d05dfbdacc13830e60ac94d3725365e8
Swapnil-Narendra-Patil <patil.swap@northeastern.edu>
2023-01-07 00:35:45 -0500

    Table created

--- interactive-ecommerce-dashboard/src/pages/Orders.jsx
+++ interactive-ecommerce-dashboard/src/pages/Orders.jsx
@@ -16,3 +16,29 @@ import {
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
+
+const Orders = () => {
+  const editing = { allowDeleting: true, allowEditing: true };
+  // return (
+  //   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+  //     <Header category="Page" title="Orders" />
+  //     <GridComponent
+  //       id="gridcomp"
+  //       dataSource={ordersData}
+  //       allowPaging
+  //       allowSorting
+  //       allowExcelExport
+  //       allowPdfExport
+  //       contextMenuItems={contextMenuItems}
+  //       editSettings={editing}
+  //     >
+  //       <ColumnsDirective>
+  //         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+  //         {ordersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+  //       </ColumnsDirective>
+  //       <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+  //     </GridComponent>
+  //   </div>
+  );
+};
+export default Orders;
