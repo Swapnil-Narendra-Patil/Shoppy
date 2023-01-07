33756553669d018eace5617b2c5d9ef131e7c967
Swapnil-Narendra-Patil <patil.swap@northeastern.edu>
2023-01-07 16:27:11 -0500

    final Commit

--- interactive-ecommerce-dashboard/src/components/Charts/Stacked.jsx
+++ interactive-ecommerce-dashboard/src/components/Charts/Stacked.jsx
@@ -1,39 +1,31 @@
-import React from "react";
-import {
-  ChartComponent,
-  SeriesCollectionDirective,
-  SeriesDirective,
-  Inject,
-  Legend,
-  Category,
-  stackingColumnseires,
-  Tooltip,
-  StackingColumnSeries,
-} from "@syncfusion/ej2-react-charts";
-import {
-  stackedCustomSeries,
-  stackedPrimaryXAxis,
-  stackedPrimaryYAxis,
-} from "../../data/dummy";
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 
-const Stacked = ({width , height }) => {
-  return(
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
+  return (
     <ChartComponent
-     widht = "300px"
-     height = {height}
-     id = "charts"
-     primaryXAxis = {stackedPrimaryXAxis}
-     primaryYAxis = {stackedPrimaryYAxis}
-    chartArea = {{border : {width : 0}}}
-    tooltip = {{enable:true}}
-    LegendSettings = {{background : 'white'}}
-     >
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]}/>
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
       <SeriesCollectionDirective>
-        {stackedCustomSeries.map ((item,index) => <SeriesDirective key={index} {...item}/>)}
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
       </SeriesCollectionDirective>
     </ChartComponent>
-  )
+  );
 };
 
 export default Stacked;
--- interactive-ecommerce-dashboard/src/pages/Orders.jsx
+++ interactive-ecommerce-dashboard/src/pages/Orders.jsx
@@ -1,18 +1,5 @@
-import React from "react";
-import {
-  GridComponent,
-  ColumnsDirective,
-  ColumnDirective,
-  Resize,
-  Sort,
-  ContextMenu,
-  Filter,
-  Page,
-  ExcelExport,
-  PdfExport,
-  Edit,
-  Inject,
-} from "@syncfusion/ej2-react-grids";
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
