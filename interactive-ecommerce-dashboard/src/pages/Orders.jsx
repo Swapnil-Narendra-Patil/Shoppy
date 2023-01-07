b5fed1098b344139924a28043b19e8657368ff47
Swapnil-Narendra-Patil <patil.swap@northeastern.edu>
2023-01-07 00:33:33 -0500

    Orders page initiated

--- interactive-ecommerce-dashboard/src/pages/Orders.jsx
+++ interactive-ecommerce-dashboard/src/pages/Orders.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
-const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
-
-export default Orders
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
