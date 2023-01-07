27ed43be1ba5047047072e8647cd2e02940b3407
Swapnil-Narendra-Patil <patil.swap@northeastern.edu>
2023-01-06 22:55:01 -0500

    Imported Stacked chart

--- interactive-ecommerce-dashboard/src/components/Charts/Stacked.jsx
+++ interactive-ecommerce-dashboard/src/components/Charts/Stacked.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesDirective,
+  SeriesCollectionDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 
 const Stacked = () => {
-  return (
-  <div>Stacked</div>
-  )
-}
+  return <div>Stacked</div>;
+};
 
 export default Stacked;
